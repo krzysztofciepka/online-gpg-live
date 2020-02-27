7d74f04ba2f43d5588dd2add0a0d0c8c02e89991
Krzysztof Ciepka <krzysztof.ciepka@blue-focus.com>
2020-02-27 16:33:55 +0100

    Release 1.0.1

---
+++ precache-manifest.9d642ea470eca2933d10b2ee348b4ba6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5447796936ee03eccc2fd5ae3533374",
+    "url": "/online-gpg-live/index.html"
+  },
+  {
+    "revision": "762b04c3756ebb792f72",
+    "url": "/online-gpg-live/static/css/2.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "679b8aaa604a2b88c5d4",
+    "url": "/online-gpg-live/static/css/main.78fa9651.chunk.css"
+  },
+  {
+    "revision": "762b04c3756ebb792f72",
+    "url": "/online-gpg-live/static/js/2.a533c9c2.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/online-gpg-live/static/js/2.a533c9c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679b8aaa604a2b88c5d4",
+    "url": "/online-gpg-live/static/js/main.522424d2.chunk.js"
+  },
+  {
+    "revision": "a1517499209d3d3df672",
+    "url": "/online-gpg-live/static/js/runtime-main.55972adf.js"
+  }
+]);
