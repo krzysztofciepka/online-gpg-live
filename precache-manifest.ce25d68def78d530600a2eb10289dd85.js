1158c138620ae6bfe15d633df5cd61ba18a832da
Krzysztof Ciepka <krzysztof.ciepka@blue-focus.com>
2020-02-27 16:09:51 +0100

    Release 1.0.0

---
+++ precache-manifest.ce25d68def78d530600a2eb10289dd85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f20dabbe48091b366634fac3f91988c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3fa0def202d02bfe6c1",
+    "url": "/static/css/2.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "feb5588bcfb9511043db",
+    "url": "/static/css/main.78fa9651.chunk.css"
+  },
+  {
+    "revision": "c3fa0def202d02bfe6c1",
+    "url": "/static/js/2.daf5060c.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.daf5060c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb5588bcfb9511043db",
+    "url": "/static/js/main.8038021c.chunk.js"
+  },
+  {
+    "revision": "4f8360966bbf1effcdf9",
+    "url": "/static/js/runtime-main.d683be26.js"
+  }
+]);
